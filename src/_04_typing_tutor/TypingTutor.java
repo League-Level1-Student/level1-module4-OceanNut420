3176d328a964e50f6fc0773fae26051f04bdb717
OceanNut420 <54760319+OceanNut420@users.noreply.github.com>
2022-06-14 16:58:47 -0700

    augh

--- src/_04_typing_tutor/TypingTutor.java
+++ src/_04_typing_tutor/TypingTutor.java
@@ -6,10 +6,15 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class TypingTutor {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.Color;
+
+public class TypingTutor implements KeyListener{
 
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
 	char currentLetter;
 	
 	char generateRandomLetter() {
@@ -20,11 +25,51 @@ public class TypingTutor {
 	void setup(){
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
 		
 		currentLetter = generateRandomLetter();
+		String curr = "" + currentLetter;
 		
-		JLabel label = new JLabel();
-		label.setText(currentLetter);
+		label.setText(curr);
+		label.setFont(label.getFont().deriveFont(28.0f));
+		label.setHorizontalAlignment(JLabel.CENTER);
+		
+		panel.add(label);
+		frame.pack();
+		frame.addKeyListener(this);
+		
+	}
+	
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		String curr = "" + currentLetter;
+		char user = e.getKeyChar();
+		String user2 = "" + user;
+		
+		System.out.println("key pressed: " + user);
+		if(curr.equals(user2)) {
+			System.out.println("correct");
+			panel.setBackground(Color.GREEN);
+		}
+		else {
+			panel.setBackground(Color.RED);
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		currentLetter = generateRandomLetter();
+		String curr = "" + currentLetter;
+		label.setText(curr);
+		
 	}
 	
 }
